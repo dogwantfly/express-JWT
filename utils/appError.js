81e91c015ba080c23bd72c12f27f5db7c697034f
hsin-yu <tina2793778@gmail.com>
2022-05-12 22:10:32 +0800

    修改 appError.js

--- utils/appError.js
+++ utils/appError.js
@@ -2,8 +2,7 @@ const appError = (httpStatus, errMessage, next) => {
   const error = new Error(errMessage);
   error.statusCode = httpStatus;
   error.isOperational = true; // 可預期錯誤
-  const status = `${httpStatus}`.startsWith('4') ? 'fail' : 'error';
-  error.status = status;
+  error.status = false;
   next(error);
 }
 
